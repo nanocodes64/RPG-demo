9646387d9e68c47a710d069b86496cade9ed529c
nanocodes64 <nanocodesyt@gmail.com>
2021-11-01 12:30:47 +0100

    got some stuff done

--- src/main.py
+++ src/main.py
@@ -1,12 +1,35 @@
 #imports
-from mod import *
+#from mod import *
+
+#Inner Classes
+class Equipment:
+    def __init__(self, dmg, name, lvl):
+        if lvl >= 1.1: 
+            self.dmg = dmg * lvl / 2
+        else:
+            self.dmg = dmg
+    def __repr__(self):
+        return f"Equipment|name:{self.name} dmg:{self.dmg}, lvl:{self.lvl}"
+        
+class Player:
+    def __init__(self, hp, lvl, equipment): 
+        self.hp = hp
+        self.lvl = lvl
+        self.eq = equipment
+    def AddEquipment(self, equipment):
+        self.eq.append(equipment)
+    def RemoveEquipment(self, equipment):
+        self.eq.remove(equipment)
+    
+        
 
 #Inner Functions
-
+def preExecution():
+    print("THIS IS PREEXECUTION")
 #Main Function
 def Main():
+    preExecution()
     print("Hello World")
-    loadfile = JSONINTERFACE("data.txt")
 
 
 
--- src/mod.py
+++ src/mod.py
@@ -5,13 +5,16 @@ class JSONINTERFACE:
     def __init__(self, current_file):
         self.cfile = current_file
     def fetch(self, toFetch):
+        chdir("mods")
         readF = open(self.cfile, "r")
         jsonF = loads(readF.read())
+        readF.close()
+        chdir("..")
         if  toFetch=="A_A_A_A":
             return jsonF
         else:
-            
             return jsonF[toFetch]
+    
 
         
             
